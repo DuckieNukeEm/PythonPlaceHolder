b3c8d6d2cb5ce34c17f3443e4daf23d98a1f6570
DuckieNukeEm <DuckieNukeEm@users.noreply.github.com>
2016-09-22 08:29:33 -0500

    updated the infile process
    updated the file so that if we want to modify a date on the save location, we can

--- Join-csv-together/JoinFiles.py
+++ Join-csv-together/JoinFiles.py
@@ -4,6 +4,7 @@ import os
 import csv
 import string
 import sys
+import datetime as dt
 
 ##############################
 #
@@ -15,7 +16,8 @@ File_location = os.path.dirname(os.path.realpath(__file__))
 file_save = os.path.dirname(os.path.realpath(__file__)) + "\\combined file.csv"
 header = []
 file_type = 'csv'
-delim = ","
+out_delim = ","
+in_delim = ","
 list_of_files = []
 replace_dict = {}
 
@@ -61,7 +63,7 @@ def load_file(head, file_name, open_type = 'rb', delimiter = ","):
             o_list = []
         else:
             o_list = o_list[l+1:]
-    elif isinstance(l[1], (int, long)): #if the header is a number (start on row header)
+    elif isinstance(head, (int, long)): #if the header is a number (start on row header)
         o_list = o_list[head:]
     else:
         1==1
@@ -82,6 +84,19 @@ def replace_values(in_list, value_to_find, value_to_replace):
         in_list[j] = [value_to_replace if x == value_to_find else x for x in in_list[j]]
         #in_list[j] = map(lambda x:x if x != value_to_find else value_to_replace,  in_list[j])
     return in_list
+
+def check_for_date_IO(file_path):
+    ###simple function that will replace certain key words ($PPE$,$TODAY$) with the proper date
+    file_path = file_path.split("$")
+    if file_path[1].upper() == "PPE":
+       file_path[1] = str(dt.date.today() - dt.timedelta(days = (dt.date.today() - dt.date(1900,01,12)).days % 14))
+       file_path = ''.join(file_path)
+    elif file_path[1].upper() == "TODAY":
+       file_path[1] = str(dt.date.today())
+       file_path = ''.join(file_path)
+    else:
+        1==1
+    return(file_path)
 
 ##############################
 #
@@ -114,9 +129,14 @@ for l in con:
     elif v == 'save location':       # if the control parameter has a save location for the output
         file_save = l[1]
       #  print(file_save)
-    elif v == 'delimiter':           # if the control parameter has a delimiter
-        delim = l[1]
-      #  print(delim)
+    elif v == 'in delimiter':           # if the control parameter has a delimiter for the input files
+        in_delim = l[1].strip()
+      #  print(in_delim)
+    elif v == 'out delimiter':             #if the control paramter has a delimiter for the output files
+        out_delim = l[1].strip()
+    elif v =='delimiter':                 # if you want a general delimiter
+        out_delim = l[1].strip()
+        in_delim = l[1].strip()
     elif v == 'file':                # if the control parameter has a file it would like to use (more than one)
         list_of_files.append(File_location + l[1])
      #   print(list_of_files)
@@ -128,7 +148,7 @@ for l in con:
      #   print(run_dats_script)
     else:
         continue
-
+print(in_delim)
 # getting and cleaning the list of files
 if(len(list_of_files) == 0):
     list_of_files = get_list_of_files(File_location)
@@ -138,24 +158,27 @@ if(len(list_of_files) == 0):
 if(len(list_of_files) == 0):
     sys.exit("Sorry buddy, but the location you gave for where the files are, there are no csv files there")
 
+#checking to see if we need to 'modify' the save location string
+file_save = check_for_date_IO(file_save)
+
 #overwriting saved file
 if isinstance(header, (int, long)) or len(header) == 0:
     open(file_save, "w").close()
 else:
     with open(file_save, "wb") as f:
-        csv_writer = csv.writer(f, delimiter = delim)
+        csv_writer = csv.writer(f, delimiter = out_delimdelim)
         csv_writer.writerow(header)
 
 for filez in list_of_files:
     print("working on file %s, %i of %i") % (filez, list_of_files.index(filez), len(list_of_files))
 
-    l_filez = load_file(header, filez, delimiter = delim)
+    l_filez = load_file(header, filez, delimiter = in_delim)
     if len(l_filez) > 0:
         if len(replace_dict) > 0:
             for k, v in replace_dict.items():
                 l_filez = replace_values(l_filez, k, v)
         with open(file_save, 'ab') as you_such_a_foo:
-            csv_writer = csv.writer(you_such_a_foo,  delimiter = delim)
+            csv_writer = csv.writer(you_such_a_foo,  delimiter = out_delim)
             for o in l_filez:
                 csv_writer.writerow(o)
 
